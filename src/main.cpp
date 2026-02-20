812f312da89210f350928865fa391a9a51f87960
RadheshyamGhosh <shambhughosh843@gmail.com>
2026-02-20 08:58:51 +0530

    DHT Setup and starting code

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,12 @@
 #include <DHT.h>
 #define DHTPIN 2     // DHT11 CONNECTED TO D2
 #define DHTTYPE DHT11   // Sensor type
-
-
+DHT dht(DHTPIN, DHTTYPE);
 void setup() {
-
+    dht.begin();
+    Serial.begin(9600);
+    dht.begin();
+    Serial.println("DHT11 Sensor initialized!");
 }
 
 void loop() {
